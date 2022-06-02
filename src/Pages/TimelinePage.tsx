f20e69bb86c2573a348bc541d033714c7a4ddb1a
Jamie-Lee Hart <71268169+pgm-jamihart@users.noreply.github.com>
2022-06-02 14:36:10 +0200

    feat: timeline pagination

--- src/Pages/TimelinePage.tsx
+++ src/Pages/TimelinePage.tsx
@@ -8,6 +8,19 @@ import { Timeline } from "../components/Timeline";
 const TimelinePage = () => {
   const [expenses, setExpenses] = useState<any[]>([]);
   const [currentCar, setCurrentCar] = useState<any>({});
+  const [from, setFrom] = useState(0);
+  const [to, setTo] = useState(5);
+  const [count, setCount] = useState(0);
+
+  const handleNext = () => {
+    setFrom(from + 5);
+    setTo(to + 5);
+  };
+
+  const handlePrev = () => {
+    setFrom(from - 5);
+    setTo(to - 5);
+  };
 
   useEffect(() => {
     const carId = localStorage.getItem("car");
@@ -20,11 +33,25 @@ const TimelinePage = () => {
     if (!currentCar.id) return;
 
     (async () => {
+      const { data: expensesCount, error: expensesErrorCount } = await supabase
+        .from("expenses")
+        .select("id")
+        .eq("car_id", currentCar.id);
+
+      if (expensesErrorCount) {
+        console.log(expensesErrorCount);
+      }
+
+      if (expensesCount) {
+        setCount(expensesCount.length);
+      }
+
       // return all expenses for the current car with the category type
       const { data: expenses, error: expensesError } = await supabase
         .from("expenses")
         .select("id, date, total, category_id")
-        .eq("car_id", currentCar.id);
+        .eq("car_id", currentCar.id)
+        .range(from, to);
 
       if (expensesError) {
         console.log(expensesError);
@@ -58,7 +85,7 @@ const TimelinePage = () => {
         );
       }
     })();
-  }, [currentCar.id]);
+  }, [currentCar.id, from, to]);
 
   console.log(expenses);
 
@@ -74,7 +101,7 @@ const TimelinePage = () => {
           </p>
         )}
 
-        {currentCar.id && expenses.length == 0 && (
+        {currentCar.id && expenses.length === 0 && (
           <p className="text-center text-gray-500">
             You have no expenses. Please add some.
           </p>
@@ -83,6 +110,10 @@ const TimelinePage = () => {
         {currentCar.id && expenses.length > 0 && (
           <div>
             <Timeline expenses={expenses} />
+
+            {from > 0 && <button onClick={handlePrev}>prev</button>}
+
+            {to < count && <button onClick={handleNext}>next</button>}
           </div>
         )}
       </div>
